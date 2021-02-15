832d46970ade9215f635fbd3d654d2f7a13a2f42
Micledson Carlos <micledson14@gmail.com>
2021-02-15 04:23:59 -0300

    a

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const routes = require("./src/routes");
 const app = express();
 
-app.use(routes);
 app.get("/", (req, res) => {
   return res.json({ rota: "main" });
 });
+app.use(routes);
+
 app.listen(process.env.PORT | 3000);
