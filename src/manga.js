64a7a9675fe713b746df9485aab8d63b93340d15
Micledson Carlos <micledson14@gmail.com>
2021-02-15 04:19:27 -0300

    url

--- src/manga.js
+++ src/manga.js
@@ -4,23 +4,24 @@ const cheerio = require("cheerio");
 async function searchManga(req, res) {
   const { manga } = req.params;
   const url = `https://mangahostz.com/find/${manga}`;
-  await axios(url)
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const search = $("tr");
-      const mangaList = [];
-      search.each(function () {
-        const linkManga = $(this).find(".pull-left").attr("href");
-        const mangaId = linkManga.split("/")[4];
-        const nome = $(this).find(".pull-left").attr("title");
-        const capa = $(this).find(".manga").attr("src");
+  return res.json({ url });
+  //   await axios(url)
+  //     .then((response) => {
+  //       const html = response.data;
+  //       const $ = cheerio.load(html);
+  //       const search = $("tr");
+  //       const mangaList = [];
+  //       search.each(function () {
+  //         const linkManga = $(this).find(".pull-left").attr("href");
+  //         const mangaId = linkManga.split("/")[4];
+  //         const nome = $(this).find(".pull-left").attr("title");
+  //         const capa = $(this).find(".manga").attr("src");
 
-        mangaList.push({ linkManga, mangaId, nome, capa });
-      });
-      return res.json(mangaList);
-    })
-    .catch(console.error);
+  //         mangaList.push({ linkManga, mangaId, nome, capa });
+  //       });
+  //       return res.json(mangaList);
+  //     })
+  //     .catch(console.error);
 }
 
 module.exports = {
