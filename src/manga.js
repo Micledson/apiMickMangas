0df8613c96b69a54da402be447ec2336e12e0437
Micledson Carlos <micledson14@gmail.com>
2021-02-15 12:49:28 -0300

    google

--- src/manga.js
+++ src/manga.js
@@ -3,21 +3,23 @@ const cheerio = require("cheerio");
 
 async function searchManga(req, res) {
   const { manga } = req.params;
-  const url = `https://mangahostz.com/find/${manga}`;
+  // const url = `https://mangahostz.com/find/${manga}`;
+  const url = "https://google.com";
   const mangas = await axios
     .get(url)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
-      const search = $("tr");
+      // const search = $("tr");
+      const search = $("div");
       const mangaList = [];
       search.each(function () {
-        const linkManga = $(this).find(".pull-left").attr("href");
-        const mangaId = linkManga.split("/")[4];
-        const nome = $(this).find(".pull-left").attr("title");
-        const capa = $(this).find(".manga").attr("src");
+        // const linkManga = $(this).find(".pull-left").attr("href");
+        // const mangaId = linkManga.split("/")[4];
+        // const nome = $(this).find(".pull-left").attr("title");
+        // const capa = $(this).find(".manga").attr("src");
 
-        mangaList.push({ linkManga, mangaId, nome, capa });
+        // mangaList.push({ linkManga, mangaId, nome, capa });
         message = "entrou aqui";
       });
       // return mangaList;
